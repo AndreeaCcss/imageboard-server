0e537fdd566150e62bd842b446dda273cdee615a
andreeaCcss <andreeaccss@gmail.com>
2019-09-06 11:31:36 +0200

    added post method

--- image/router.js
+++ image/router.js
@@ -8,4 +8,10 @@ router.get("/images", (req, res, next) => {
     .catch(err => next(err));
 });
 
+router.post("/images", (req, res, next) => {
+  Image.create(req.body)
+    .then(image => res.json(image))
+    .catch(next);
+});
+
 module.exports = router;
--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
-const db = require("./db");
-const imageModel = require("./image/model");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+// const db = require("./db");
+// const imageModel = require("./image/model");
 const imageRouter = require("./image/router");
+
 const app = express();
+const corsMiddleware = cors();
+app.use(corsMiddleware);
+
+const parserMiddleware = bodyParser.json();
+app.use(parserMiddleware);
 
 app.use(imageRouter);
 const port = process.env.PORT || 4000;
